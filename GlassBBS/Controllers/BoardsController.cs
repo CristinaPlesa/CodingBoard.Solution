49bf5c570255fd1aabba0687c322c507cba0d0de
Usarneme <jamestcoop@gmail.com>
2021-06-09 11:04:11 -0700

    final touches before fork

--- GlassBBS/Controllers/BoardsController.cs
+++ GlassBBS/Controllers/BoardsController.cs
@@ -43,15 +43,48 @@ namespace GlassBBS.Controllers
     [HttpPost("/api/board/{boardId}/posts/new")]
     public async Task<ActionResult<Post>> NewPost(string body, string postAuthorId, string boardId)
     {
-      Console.WriteLine("HIT POST NEW POST ROUTE {0} {1} {2}", boardId, body, postAuthorId);
       Post newPost = new() { Body = body, PostAuthorId = postAuthorId, BoardId = boardId };
-      Console.WriteLine("NEW POST GENERATED {0}", newPost);
       _db.Posts.Add(newPost);
-      Console.WriteLine("AFTER ADD");
       await _db.SaveChangesAsync();
-      Console.WriteLine("AFTER SAVE...");
       return CreatedAtAction("NewPost", new { id = newPost.PostId }, newPost);
+    }
 
+    [HttpDelete("/api/board/{boardId}/posts/delete/{postId}")]
+    public async Task<IActionResult> DeletePost(string postId)
+    {
+      Post thisPost = _db.Posts.FirstOrDefault(p => p.PostId == postId);
+      _db.Posts.Remove(thisPost);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+
+    [HttpPut("/api/posts/edit/{postId}")]
+    public async Task<IActionResult> EditPost(string postId, Post thePost)
+    {
+      Console.WriteLine("HIT PUT edit POSTS ROUTE {0} {1} ", postId, thePost);
+      // Post thisPost = _db.Posts.FirstOrDefault(p => p.PostId == postId);
+      _db.Entry(thePost).State = EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (PostExists(postId))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    private bool PostExists(string postId)
+    {
+      return _db.Posts.Any(p => p.PostId == postId);
     }
   }
 }
